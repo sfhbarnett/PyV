79f02fa53b09b70164bb78d4fa4682eae8b6b302
sbarnett <sfhbarnett91@gmail.com>
2022-07-21 16:52:00 +0200

    Can open image files, scroll through them and edit contrast

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from PyV.PIV import PIV
-from tiffstack import TiffStack
+from tiffstack import tiffstack
 import matplotlib.pyplot as plt
 from piv_filters import localfilt
 from naninterp import naninterp
 from matplotlib.figure import Figure
 from matplotlib.backends.backend_qtagg import FigureCanvasQTAgg, NavigationToolbar2QT
 from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtGui import QFontDatabase, QAction, QIcon
 import sys
 from superqt import QLabeledRangeSlider
 
@@ -22,12 +23,13 @@ class MainWindow(QtWidgets.QMainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setWindowTitle("PyV")
-        self.layout = QtWidgets.QHBoxLayout()
+
         self.leftcontrols = QtWidgets.QVBoxLayout()
         self.centralcanvas = QtWidgets.QVBoxLayout()
         self.rightcontrols = QtWidgets.QVBoxLayout()
 
-        # Left controls
+        # Left controls =========================
+
         self.toggleroi = QtWidgets.QPushButton('Roi OFF ')
         #self.toggleroi.clicked.connect(self.toggleroimode)
         self.toggleroi.setToolTip('Toggle ROI mode on/off')
@@ -57,12 +59,12 @@ class MainWindow(QtWidgets.QMainWindow):
         self.buttonbox.addStretch(1)
         self.leftcontrols.addLayout(self.buttonbox)
 
-        # Central Image controls
+        # Central Image controls ==========================
 
         self.sc = MplCanvas(self, width=7, height=8, dpi=100)
         self.filename = None
         path = '/Users/sbarnett/Documents/PIVData/fatima/ForSam/monolayer2/C1-20210708_MCF10ARAB5A_H2BGFP_Monolayer_Doxy_withoutDoxy.czi - 20210708_MCF10ARAB5A_H2BGFP_Monolayer_Doxy_withoutDoxy.czi #21.tif'
-        self.imstack = TiffStack(path)
+        self.imstack = tiffstack(path)
         self.plothandle = None
         self.sc.axes.get_xaxis().set_visible(False)
         self.sc.axes.get_yaxis().set_visible(False)
@@ -74,30 +76,117 @@ class MainWindow(QtWidgets.QMainWindow):
         self.contrastslider.setHandleLabelPosition(QLabeledRangeSlider.LabelPosition.LabelsBelow)
         self.contrastslider.label_shift_x = 10
         self.contrastslider.setRange(0, 200)
-        #self.contrastslider.valueChanged.connect(self.update_contrast)
+        self.contrastslider.valueChanged.connect(self.update_contrast)
         self.imagecontrols = QtWidgets.QHBoxLayout()
         self.imagecontrols.addWidget(self.contrastslider)
         self.imagecontrols.addWidget(self.sc)
 
         self.centralcanvas.addLayout(self.imagecontrols)
 
-        # Right controls
+        # Right controls ======================
 
-        # Combine
-        self.layout.addLayout(self.leftcontrols)
-        self.layout.addLayout(self.centralcanvas)
-        self.layout.addLayout(self.rightcontrols)
+        # Toolbar===================
+        self.mpl_toolbar = NavigationToolbar2QT(self.sc, None)
+        zoomact = QAction(QIcon("PyV/icons/zoom2.png"), "zoom", self)
+        zoomact.setCheckable(True)
+        zoomact.setShortcut("Ctrl+z")
+        zoomact.triggered.connect(self.mpl_toolbar.zoom)
+
+        panact = QAction(QIcon("PyV/icons/pan.png"),"pan",self)
+        panact.setCheckable(True)
+        panact.setShortcut("Ctrl+p")
+        panact.setChecked(False)
+        panact.triggered.connect(self.mpl_toolbar.pan)
+
+        openact = QAction(QIcon("PyV/icons/open.png"),"open file",self)
+        openact.setCheckable(False)
+        openact.triggered.connect(self.get_file)
+
+        saveact = QAction(QIcon("PyV/icons/save.png"),"Save drift corrected data",self)
+        saveact.triggered.connect(self.savedrift)
+
+        self.toolbar = self.addToolBar("zoom")
+        self.toolbar.addAction(panact)
+        self.toolbar.addAction(zoomact)
+        self.toolbar.addAction(openact)
+        self.toolbar.addAction(saveact)
+
+        # Stack slider ================
+
+        sliderholder = QtWidgets.QHBoxLayout()
+        self.stackslider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        self.stackslider.setRange(0, self.imstack.nfiles - 1)
+        self.stackslider.valueChanged.connect(self.move_through_stack)
+        self.currentimage = 0
+        sliderholder.addWidget(self.stackslider)
+        self.label = QtWidgets.QLabel('0', self)
+        self.label.setAlignment(QtCore.Qt.AlignCenter | QtCore.Qt.AlignVCenter)
+        self.label.setMinimumWidth(40)
+
+        sliderholder.addWidget(self.label)
+
+        # Combine =====================
+
+        self.layout = QtWidgets.QVBoxLayout()
+        self.centrallayout = QtWidgets.QHBoxLayout()
+        self.centrallayout.addLayout(self.leftcontrols)
+        self.centrallayout.addLayout(self.centralcanvas)
+        self.centrallayout.addLayout(self.rightcontrols)
+        self.layout.addWidget(self.toolbar)
+        self.layout.addLayout(self.centrallayout)
+        self.layout.addLayout(sliderholder)
         widget = QtWidgets.QWidget()
         widget.setLayout(self.layout)
         self.setCentralWidget(widget)
         self.setGeometry(100, 100, 1200, 900)
+
+    def get_file(self):
+        self.sc.axes.cla()
+        self.filename = None
+        self.filename = QtWidgets.QFileDialog.getOpenFileName(self, 'Open File',directory='~/Documents')
+        if self.filename[0] != "":
+            self.filename = self.filename[0]
+            self.imstack = tiffstack(self.filename)
+            self.plothandle = self.sc.axes.imshow(self.imstack.getimage(0))
+            self.stackslider.setRange(0, self.imstack.nfiles - 1)
+            self.stackslider.setValue(0)
+            self.plothandle.set_cmap('gray')
+            self.sc.fig.canvas.draw()
+            self.contrastslider.setRange(0, self.imstack.maximum * 1.5)
+            self.contrastslider.setValue((self.imstack.minimum, self.imstack.maximum))
+
+    def savedrift(self):
+        pass
+
+    def move_through_stack(self, value):
+        """ Updates the current image in the viewport"""
+
+        self.plothandle.set_data(self.imstack.getimage(value))
+
+        # Check and update contrast
+        if not self.autocontrast.isChecked():
+            self.plothandle.set_clim([self.mincontrast, self.maxcontrast])
+        else:
+            self.contrastslider.setRange(0, self.imstack.maximum*1.5)
+            self.contrastslider.setValue((self.imstack.minimum, self.imstack.maximum))
+
+        self.sc.fig.canvas.draw()
+        self.label.setText(str(value))
+        self.currentimage = value
+
+    def update_contrast(self, value):
+        self.mincontrast = value[0]
+        self.maxcontrast = value[1]
+        if self.plothandle:
+            self.plothandle.set_clim(self.mincontrast, self.maxcontrast)
+            self.sc.fig.canvas.draw()
 
 
 
 
 path = '/Users/sbarnett/Documents/PIVData/fatima/ForSam/monolayer2/C1-20210708_MCF10ARAB5A_H2BGFP_Monolayer_Doxy_withoutDoxy.czi - 20210708_MCF10ARAB5A_H2BGFP_Monolayer_Doxy_withoutDoxy.czi #21.tif'
 
-tf = TiffStack(path)
+tf = tiffstack(path)
 
 # plt.imshow(tf.getimage(0))
 # plt.show()
--- tiffstack.py
+++ tiffstack.py
@@ -1,21 +1,64 @@
+import tifffile
+import numpy as np
+from skimage.transform import AffineTransform, warp
 
-import tifffile as tf
+class tiffstack():
 
-
-class TiffStack:
     """
-    Tiffstack holds information about the images to process and accesses them in a memory-efficient manner
-    improvements: implement a TiffFile class for each image to more readily access parameters such as width/height
-    :param pathname to the tif image
+    Container class for a stack of tiff images
+    to do: handle both multipage tiffs as well as image sequences
     """
-    def __init__(self, pathname):
-        self.ims = tf.TiffFile(pathname)
+
+    def __init__(self, pathname=None):
+        self.ims = None
+        self.nfiles = 0
+        self.minimum = 0
+        self.maximum = np.inf
+        self.width = 0
+        self.height = 0
+        self.dtype = None
+        self.pathname = pathname
+        if pathname is not None:
+            self.load_info(pathname)
+        self.transforms = []
+
+    def load_info(self, pathname):
+        self.ims = tifffile.TiffFile(pathname)
+        self.width = self.ims.pages[0].shape[0]
+        self.height = self.ims.pages[0].shape[1]
+        self.dtype = self.ims.pages[0].dtype
         self.nfiles = len(self.ims.pages)
-        page = self.ims.pages[0]
-        self.width = page.shape[0]
-        self.height = page.shape[1]
 
     def getimage(self, index):
-        img = self.ims.pages[index].asarray()
-        return img
+        """
+        Load in the image at index and store the min and max values
+        :param index: which image in series to open
+        :return: numpy array of image
+        """
+        image = self.ims.pages[index].asarray()
+        self.minimum = image.min()
+        self.maximum = image.max()
+        return image
 
+    def settransforms(self, xshift, yshift):
+        """
+        Once drift has been estimated, generate an affine transform for each image
+        """
+        self.transforms.append(AffineTransform(translation=[0, 0]))
+        for i in range(self.nfiles-1):
+            self.transforms.append(AffineTransform(translation=[xshift[i], yshift[i]]))
+
+    def printtransforms(self):
+        print(self.nfiles, len(self.transforms))
+        for i in range(len(self.transforms)):
+            print(self.transforms[i])
+
+    def savedriftcorrected(self):
+        outname = self.pathname[:-4] + 'DC.tif'
+        with tifffile.TiffWriter(outname) as tif:
+            for index in range(self.nfiles):
+                image = self.getimage(index)
+                transform = self.transforms[index]
+                shifted = warp(image, transform, preserve_range=True)
+                tif.save(np.int16(shifted))
+        print('saved data')
