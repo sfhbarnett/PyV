6609348b25d69afd8b8227bb831ffcd6858a06ea
sbarnett <sfhbarnett91@gmail.com>
2022-08-05 12:37:41 +0200

    added linearise field and show image box

--- main.py
+++ main.py
@@ -41,7 +41,7 @@ class externalPIV(QtCore.QThread):
             self.u[:, :, frame] = u
             self.v[:, :, frame] = v
             self.progressstatus.emit(frame)
-        return self.x,self.y,self.u,self.v
+        return self.x, self.y, self.u, self.v
 
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
@@ -49,13 +49,17 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
         self.setupUi(self)
+        self.setWindowTitle("PyV - v0.1")
         self.seticons()
         self.piv = Pivdata()
         self.mincontrast = 0
         self.maxcontrast = 64000
         self.quiver = None
         self.imagehandle = plt.imshow(np.array([[]]))
+        self.imagehandle.set_cmap('gray')
         self.extent = [0, 1000, 0, 1000]
+        self.imstack = None
+        self.showQuiver = False
         self.mpl_toolbar = NavigationToolbar2QT(self.mplwidget.canvas, None)
         self.connectsignalsslots()
         self.stackslider.setRange(0, 10)
@@ -82,7 +86,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.solidcode = {'black':'k','green':'g','red':'r','cyan':'c','magenta':'m','yellow':'y'}
         self.fieldcolormapinput.addItems(self.colormaps+self.solids)
         self.quivercmap = 'jet'
-        self.quivercolor =None
+        self.quivercolor = None
         self.progressbar = QtWidgets.QProgressBar()
         self.statusbar.addPermanentWidget(self.progressbar)
         self.alignmentbutton.setStyleSheet("")
@@ -125,6 +129,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.vrmsbutton.clicked.connect(self.calculatevrms)
         self.linearorderbutton.clicked.connect(self.calculateLinearOrderParameter)
         self.fieldcolormapinput.currentIndexChanged.connect(self.changecmap)
+        self.showimagecheck.stateChanged.connect(self.showImage)
 
     def get_file(self):
         self.mplwidget.canvas.axes.cla()
@@ -146,21 +151,9 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def move_through_stack(self, value):
         """ Updates the current image in the viewport"""
         self.currentimage = value
-        if self.imagehandle is not None:
-            self.imagehandle.set_cmap('gray')
-            self.stackpos.setText(str(self.currentimage+1))
-            self.imagehandle.set_data(self.imstack.getimage(value))
-            self.imagehandle.set_extent(self.extent)
-            self.mplwidget.canvas.figure.canvas.draw_idle()
-            # Check and update contrast
-            if not self.autocontrast.isChecked():
-                self.imagehandle.set_clim([self.mincontrast, self.maxcontrast])
-            else:
-                self.contrastslider.setRange(0, self.imstack.maximum*1.5)
-                self.contrastslider.setValue((self.imstack.minimum, self.imstack.maximum))
+        self.showImage()
         if self.quiver is not None:
             self.makeQuiver()
-
 
     def update_contrast(self, value):
         self.mincontrast = value[0]
@@ -188,6 +181,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.piv.y[:, :, frame] = y * self.pixelsize
             self.piv.u[:, :, frame] = u * self.pixelsize / self.timeinterval
             self.piv.v[:, :, frame] = v * self.pixelsize / self.timeinterval
+        self.showQuiver = True
         print(time.time()-start)
         self.makeQuiver()
 
@@ -195,7 +189,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def makeQuiver(self):
         if self.quiver is not None:
             self.quiver.remove()
-        if self.currentimage != 0:
+        if self.currentimage != 0 and self.showQuiver == True:
             frame = self.currentimage-1
             M = np.sqrt(self.piv.u[:, :, frame]*self.piv.u[:, :, frame]+self.piv.v[:, :, frame]*self.piv.v[:, :, frame])
             clipM = np.clip(M, 0, self.maxdispspeed)
@@ -204,7 +198,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                                                                 self.piv.y[:, :, frame]+1,
                                                                 self.piv.u[:, :, frame]*self.arrowscale,
                                                                 self.piv.v[:, :, frame]*self.arrowscale, clipM,
-                                                                scale_units='xy',scale=1,
+                                                                scale_units='xy', scale=1,
                                                                 cmap=self.quivercmap)
             else:
                 self.quiver = self.mplwidget.canvas.axes.quiver(self.piv.x[:, :, frame]+1,
@@ -212,7 +206,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                                                                 self.piv.u[:, :, frame]*self.arrowscale,
                                                                 self.piv.v[:, :, frame]*self.arrowscale,
                                                                 color=self.quivercolor,
-                                                                scale_units='xy',scale=1)
+                                                                scale_units='xy', scale=1)
         else:
             self.quiver = self.mplwidget.canvas.axes.quiver([], [], [], [])
         self.imagehandle.set_extent(self.extent)
@@ -252,7 +246,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.makeQuiver()
         self.setFocus()
 
-    def changecmap(self,index):
+    def changecmap(self, index):
         cmap = self.fieldcolormapinput.currentText()
         if cmap in self.colormaps:
             self.quivercmap = cmap
@@ -284,34 +278,38 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def alignment(self):
         if self.currentimage != 0:
             frame = self.currentimage-1
-            averageU = np.mean(self.piv.u[:,:,frame])
-            averageV = np.mean(self.piv.v[:,:,frame])
+            averageU = np.mean(self.piv.u[:, :, frame])
+            averageV = np.mean(self.piv.v[:, :, frame])
             meanvector = np.array([averageU,averageV])
-            upper = np.vstack((self.piv.u[:,:,frame].flatten('F'),self.piv.v[:,:,frame].flatten('F'))).T@meanvector
+            upper = np.vstack((self.piv.u[:, :, frame].flatten('F'),self.piv.v[:, :, frame].flatten('F'))).T@meanvector
             lower = np.sqrt(self.piv.u[:, :, frame].flatten('F')**2+self.piv.v[:, :, frame].flatten('F')**2) * np.sqrt(meanvector[0]**2+meanvector[1]**2)
-            value = np.divide(upper,lower)
-            map = np.reshape(value,(63,63))
-            map = resize(map,(self.piv.u.shape[0]*self.windowsize//2,self.piv.u.shape[1]*self.windowsize//2),order=0,preserve_range=True)
+            value = np.divide(upper, lower)
+            map = np.reshape(value, (63, 63))
+            map = resize(map, (self.piv.u.shape[0]*self.windowsize//2, self.piv.u.shape[1]*self.windowsize//2), order=0, preserve_range=True)
             self.imagehandle.set_data(map)
             self.contrastslider.setRange(-1, 1)
-            self.contrastslider.setValue((-1,1))
+            self.contrastslider.setValue((-1, 1))
             self.contrastslider.setSingleStep(0.01)
             self.contrastslider.setDecimals(2)
+            self.contrastslider.label_shift_x = 10
             self.extent = [self.windowsize/4, self.imstack.width-self.windowsize/4, self.windowsize/4, self.imstack.width-self.windowsize/4]
             self.imagehandle.set_extent(self.extent)
             self.imagehandle.set_cmap('viridis')
             self.imagehandle.set_clim(-1, 1)
             self.mplwidget.canvas.fig.canvas.draw()
 
-
     def orientation(self):
-        rho = np.sqrt(self.piv.u[:, :, self.currentimage]**2+self.piv.v[:, :, self.currentimage]**2)
-        phi = np.arctan2(self.piv.v[:, :, self.currentimage], self.piv.u[:, :, self.currentimage])
-        theta = resize(np.rad2deg(phi), (self.piv.u.shape[0]*self.windowsize//2,self.piv.u.shape[1]*self.windowsize//2), order=1, preserve_range=True)
-        self.imagehandle.set_data(theta+180)
-        self.imagehandle.set_clim(0, 360)
-        self.imagehandle.set_cmap('hsv')
-        self.mplwidget.canvas.fig.canvas.draw()
+        if self.currentimage != 0:
+            frame = self.currentimage-1
+            rho = np.sqrt(self.piv.u[:, :, frame]**2+self.piv.v[:, :, frame]**2)
+            phi = np.arctan2(-self.piv.v[:, :, frame], self.piv.u[:, :, frame])
+            theta = resize(np.rad2deg(phi), (self.piv.u.shape[0]*self.windowsize//2, self.piv.u.shape[1]*self.windowsize//2), order=0, preserve_range=True)
+            self.imagehandle.set_data(theta+180)
+            self.imagehandle.set_clim(0, 360)
+            self.extent = [self.windowsize/4, self.imstack.width-self.windowsize/4, self.windowsize/4, self.imstack.width-self.windowsize/4]
+            self.imagehandle.set_extent(self.extent)
+            self.imagehandle.set_cmap('hsv')
+            self.mplwidget.canvas.fig.canvas.draw()
 
     def importfields(self):
         """ import files that represents fields (Mx4) where M is the number of vectors. Col 0 is x, Col 1 is y,
@@ -333,6 +331,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.piv.u[:, :, counter] = np.reshape(array[:,2],[int(np.max(array[:,0]/(array[0,0]+1)))+1,int(np.max(array[:,1]/(array[0,1]+1)))+1])
                 self.piv.v[:, :, counter] = np.reshape(array[:,3],[int(np.max(array[:,0]/(array[0,0]+1)))+1,int(np.max(array[:,1]/(array[0,1]+1)))+1])
                 counter += 1
+        self.showQuiver = True
         self.makeQuiver()
 
     def calculatevrms(self):
@@ -343,19 +342,29 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
             self.vrms.append(np.sqrt(np.nanmean(velocities**2)))
         self.tableview.addData(self.vrms,'Vrms')
 
-    def rotacity(self):
-        pass
+    def showImage(self):
+        if self.imstack is not None and self.showimagecheck.isChecked():
+            self.stackpos.setText(str(self.currentimage+1))
+            self.imagehandle.set_data(self.imstack.getimage(self.currentimage))
+            self.imagehandle.set_extent(self.extent)
+            # Check and update contrast
+            if not self.autocontrast.isChecked():
+                self.imagehandle.set_clim([self.mincontrast, self.maxcontrast])
+            else:
+                self.contrastslider.setRange(0, self.imstack.maximum*1.5)
+                self.contrastslider.setValue((self.imstack.minimum, self.imstack.maximum))
+        else:
+            self.imagehandle.set_data(np.array([[], []]))
+        self.mplwidget.canvas.figure.canvas.draw_idle()
 
     def calculateLinearOrderParameter(self):
         self.LOP = []
         for frame in range(self.piv.u.shape[2]):
-            mean_u = np.nanmean(self.piv.u[:,:,frame])
-            mean_v = np.nanmean(self.piv.v[:,:,frame])
-            mean_uv2 = np.nanmean(self.piv.u[:,:,frame]**2 + self.piv.v[:,:,frame]**2)
+            mean_u = np.nanmean(self.piv.u[:, :, frame])
+            mean_v = np.nanmean(self.piv.v[:, :, frame])
+            mean_uv2 = np.nanmean(self.piv.u[:, :, frame]**2 + self.piv.v[:, :, frame]**2)
             self.LOP.append((mean_u**2+mean_v**2)/mean_uv2)
-        self.tableview.addData(self.LOP,'LinearOrder')
-
-
+        self.tableview.addData(self.LOP, 'LinearOrder')
 
     def natsort(self,tosort):
         """Natural sort file strings"""
@@ -397,7 +406,9 @@ class TableView(QtWidgets.QTableWidget):
                 col += 1
         self.tablewidget.show()
         
-    def addData(self,data,headerlabel):
+    def addData(self, data, headerlabel):
+        if headerlabel in self.headerlabels:
+            pass # deal with adding data to the same column
         self.headerlabels.append(headerlabel)
         col = self.tablewidget.columnCount()
         self.tablewidget.setColumnCount(col+1)
@@ -406,7 +417,7 @@ class TableView(QtWidgets.QTableWidget):
             self.tablewidget.setRowCount(len(data))
         row = 0
         for el in data:
-            cell = QtWidgets.QTableWidgetItem(str(round(el,4)))
+            cell = QtWidgets.QTableWidgetItem(str(round(el, 4)))
             self.tablewidget.setItem(row, col, cell)
             row += 1
 
@@ -417,9 +428,44 @@ class Pivdata():
         self.y = []
         self.u = []
         self.v = []
+        self.ru = []
+        self.rv = []
+        self.width = 0
+        self.height = 0
+        self.nfields = 0
         self.pixelsize = 1
         self.timeinterval = 1
         self.windowsize = 32
+
+    def rotaticity(self, x, y, u, v, cx, cy):
+        x = x-cx
+        y = y-cy
+        theta = np.rad2deg(np.arctan2(y,x))+180
+        fromN = np.deg2rad(270-theta)
+        rotmat = np.array([[np.cos(fromN), -np.sin(fromN)], [np.sin(fromN), np.cos(fromN)]])
+        rotuv = rotmat@np.array([[u], [v]])
+        angle = np.rad2deg(np.arctan2(rotuv[1], rotuv[0])) + 180
+        if angle < 90:
+            xcomponent = -1 * (1 - angle / 90)
+            ycomponent = -np.sqrt(1 - xcomponent ** 2)
+        elif 90 <= angle < 180:
+            xcomponent = (angle - 90) / 90
+            ycomponent = -1 * np.sqrt(1 - xcomponent ** 2)
+        elif 180 <= angle < 270:
+            xcomponent = 1 - (angle - 180) / 90
+            ycomponent = np.sqrt(1 - xcomponent ** 2)
+        elif angle >= 270:
+            xcomponent = -1 * ((angle - 270) / 90)
+            ycomponent = np.sqrt(1 - xcomponent ** 2)
+        return xcomponent, ycomponent
+
+    def lineariseField(self):
+        for ff in range(self.nfields):
+            for xx in range(self.height):
+                for yy in range(self.width):
+                    xcomp, ycomp = self.rotacity(self.x[xx,yy,ff],self.y[xx,yy,ff],self.u[xx,yy,ff],self.v[xx,yy,ff],self.width/2,self.height/2)
+                    self.ru[xx,yy,ff] = xcomp
+                    self.rv[xx,yy,ff] = ycomp
 
 
 
